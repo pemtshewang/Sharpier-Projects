449eca9f3853a8905934ffdb14b4bceba7409fdd
pemtshewang <pemtshewang500@gmail.com>
2024-06-23 22:03:03 +0600

    added

--- Program.cs
+++ Program.cs
@@ -11,14 +11,19 @@ Dictionary<string, string> words = new Dictionary<string, string>
 
 int lives = 5;
 bool winGame = false;
+bool correctPrediction = false;
 int randIndex = new Random().Next(1, words.Count());
 List<string> keys = words.Keys.ToList();
 string randomKey = keys[randIndex];
 
 void reduceLives()
 {
+    if (lives < 1)
+    {
+        Console.WriteLine("You Lost, Try again");
+    }
     lives--;
-    Console.WriteLine("Your Life is getting reduced");
+    Console.WriteLine("You lost a live");
 }
 
 StringBuilder sb = new StringBuilder("", randIndex);
@@ -27,8 +32,65 @@ for (int i = 0; i < randomKey.Length; i++)
     sb.Append("+");
 }
 
+bool fillChar(char ch)
+{
+    if (randomKey.Contains(ch))
+    {
+        for (int i = 0; i < randomKey.Length; i++)
+        {
+            if (randomKey[i] == ch)
+            {
+                sb[i] = ch;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+bool checkInput(string word)
+{
+    if (word.Length < 2)
+    {
+        char ch = word[0];
+        if (!fillChar(ch))
+        {
+            return false;
+        }
+        return true;
+    }
+    if (!String.IsNullOrEmpty(word))
+    {
+        if (
+            (word.ToString() == randomKey.ToString())
+            || (word.ToLower().ToString() == randomKey.ToLower().ToString())
+        )
+        {
+            correctPrediction = true;
+            return true;
+        }
+        return false;
+    }
+    return false;
+}
+
 while (!winGame)
 {
+    if (lives < 1)
+    {
+        Console.WriteLine("You Lost :x Tryagain next time;");
+        break;
+    }
+    if (sb.ToString() == randomKey.ToString())
+    {
+        winGame = true;
+    }
+    if (winGame || correctPrediction)
+    {
+        Console.WriteLine("Correct Prediction of the word: " + randomKey);
+        Console.WriteLine("You won the game");
+        break;
+    }
     Console.WriteLine("Fill the letters by Guessing the programming language! :)");
     Console.WriteLine("");
     Console.WriteLine("Hint: " + words[randomKey]);
@@ -38,9 +100,17 @@ while (!winGame)
     }
     Console.WriteLine("Guess a letter or word");
     Console.WriteLine("Your progress: " + sb.ToString());
-    string input = Console.ReadLine();
+    string? input = Console.ReadLine();
     if (String.IsNullOrEmpty(input))
     {
         Console.WriteLine("The input cannot be empty");
+        reduceLives();
+    }
+    else
+    {
+        if (!checkInput(input))
+        {
+            reduceLives();
+        }
     }
 }
--- obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45678b467d86d7ee93b5f82848b4dfcd48df735f")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
